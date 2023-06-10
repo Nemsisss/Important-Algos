45bc05e3b6bdd71692fcd67638572045896551cd
Nemsiss Shahbazian <nemsisss@usc.edu>
2023-06-10 16:37:26 -0700

    DP approach (O(n^2))

--- Longest_increasing_subsequence(LIS).py
+++ Longest_increasing_subsequence(LIS).py
@@ -1,17 +1,12 @@
-def lis(nums):
-    tails = [0] * len(nums)
-    size = 0
-    for x in nums:
-        i, j = 0, size
-        while i != j:
-            m = (i + j) // 2
-            if tails[m] < x:
-                i = m + 1
-            else:
-                j = m
-        tails[i] = x
-        size = max(i + 1, size)
-    return size
-
-arr = [10, 22, 9, 33, 21, 50, 41, 60, 80]
-print(lis(arr))
+def longest_increasing_subsequence(arr):
+    n = len(arr)
+    dp = [1] * n
+    for i in range(1, n):
+        for j in range(i):
+            if arr[j] < arr[i]:
+                dp[i] = max(dp[i], dp[j]+1)
+    return max(dp)
+ 
+ 
+arr = [10, 22, 9, 33, 21, 50, 41, 60]
+print( longest_increasing_subsequence(arr))
